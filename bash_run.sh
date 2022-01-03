7c80a1fa86d885b292641e58957d493fdc03cfed
Rishabh Jain <rishucoding@gmail.com>
2022-01-03 03:17:26 -0500

    updating script for three runs

--- bash_run.sh
+++ bash_run.sh
@@ -1,3 +1,9 @@
-nvcc -std=c++11 dataset.cpp seqalgo.cu -o seqalgo
-./seqalgo > log
-vim log
+nvcc -std=c++11 dataset.cpp seqalgo_parallel.cu -o seqalgo
+#g++ -std=c++11 dataset.cpp seqalgo.cpp -o seqalgo
+for x in 1 2 3
+do
+    nvprof ./seqalgo
+done
+ls -la frequent_itemsets_naive.txt frequent_itemsets.txt
+#./seqalgo > log
+#vim log
